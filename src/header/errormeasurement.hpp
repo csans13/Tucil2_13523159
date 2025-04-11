ece92f7378f391cdb48cec84819c9ff6fb273dc4
csans13 <167492787+csans13@users.noreply.github.com>
2025-04-11 09:05:21 +0700

    feat: add ErrorMeasurement component

---
+++ src/header/errormeasurement.hpp
@@ -0,0 +1,19 @@
+#ifndef _ERRORMEASUREMENT_HPP 
+#define _ERRORMEASUREMENT_HPP 
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "quadtree.hpp"
+
+using namespace std;
+
+namespace ErrorMeasurement { 
+    RGB computeAvgColor(const vector<vector<RGB>>& image, int x, int y, int width, int height);
+    float computeVariance(const vector<vector<RGB>>& image, int x, int y, int width, int height); 
+    float computeMAD(const vector<vector<RGB>>& image, int x, int y, int width, int height); 
+    float computeMaxPixelDiff(const vector<vector<RGB>>& image, int x, int y, int width, int height); 
+    float computeEntropy(const vector<vector<RGB>>& image, int x, int y, int width, int height); 
+}
+
+#endif
